3e27a4d2f02d042de17a43888ca5929f187c601a
Alex-IIT <effortelevationofficial@gmail.com>
2026-05-03 19:28:43 +0530

    Signup and Signin added

--- Oops_project.py
+++ Oops_project.py
@@ -1,6 +1,6 @@
 class chatbook:
     def __init__(self):
-        self.user=""
+        self.username=""
         self.password=""
         self.loggedin=False
         self.menu()
@@ -9,15 +9,36 @@ class chatbook:
          1. Press 1 to signup
          2. Press 2 to login
          3. Press 3 to write a post
-         4. Press 4 to message a friend""")
+         4. Press 4 to message a friend\n""")
         if user_input=="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.login()
         elif user_input=="3":
             pass    
         elif user_input=="4":
             pass
         else:
             exit()
+    def signup(self):
+        email= input("Enter your username->")
+        pwd=input("Enter your password->")
+        self.username=email
+        self.password=pwd
+        print("Signed Up successfully")
+        self.menu()
+    def login(self):
+        if self.username=='' and self.password=="":
+            print("Please sign up first by pressing 1 in the main menu")
+        else:
+            uname=input("Enter your username->")
+            pwd=input("Enter your password->")
+            if self.username==uname and self.password==pwd:
+                print("Logged in successfully !!") 
+                self.loggedin=True
+            else:
+                print("Invalid Credentials")
+        print("\n")
+        self.menu()
+
 chatbook1=chatbook()
